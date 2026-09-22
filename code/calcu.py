6dc6c0b04ad554ce421858ae6807049de44e5c2b
KrozFu <carlosuefagh@gmail.com>
2026-09-22 14:22:48 -0500

    feat: update changes en files

--- code/calcu.py
+++ code/calcu.py
@@ -10,10 +10,19 @@ def multi(valor1, valor2):
     return valor1 * valor2
 
 
+def divi(valor1, valor2):
+    if valor2 != 0:
+        return valor1 / valor2
+    else:
+        return "Operacion no permitida sobre 0"
+
+
 resultado_suma = suma(2, 5)
 resultado_resta = resta(2, 5)
 resultado_multi = multi(2, 5)
+resultado_divi = divi(2, 5)
 
 print(f"Resultado Suma: {resultado_suma}")
 print(f"Resultado Resta: {resultado_resta}")
 print(f"Resultado Multiplicacion: {resultado_multi}")
+print(f"Resultado Division: {resultado_divi}")
